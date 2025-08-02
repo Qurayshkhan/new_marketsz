bf1ed725eb5b4d52d1997c3c448bcbf600ed7e6a
Qurayshkhan <96570166+Qurayshkhan@users.noreply.github.com>
2025-08-02 15:31:20 +0500

    fix: remove title

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,7 +12,7 @@ import 'vue-toastification/dist/index.css';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 import helpers from './helper';
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,

a457f116501926a3d91cb337bec67aebf6740f2e
Qurayshkhan <96570166+Qurayshkhan@users.noreply.github.com>
2025-08-05 01:26:30 +0500

    fix: customer sidebar

--- resources/js/Layouts/Partials/CustomerSidebar.vue
+++ resources/js/Layouts/Partials/CustomerSidebar.vue
@@ -33,6 +33,35 @@ const props = defineProps({
                         <i class="fa-solid fa-house w-5 h-5 mr-3"></i>
                         My Suite
                     </Link>
+                    <Link
+                        href="#"
+                        class="flex items-center p-3 rounded-md hover:bg-gray-700 transition-colors duration-200"
+                    >
+                        <i class="fa-solid fa-cart-shopping w-5 h-5 mr-3"></i>
+                        My Shipments
+                    </Link>
+                    <Link
+                        :href="route('customer.account.profile')"
+                        class="flex items-center p-3 rounded-md hover:bg-gray-700 transition-colors duration-200"
+                        :class="{
+                            'text-white bg-gray-700': $page.url.startsWith(
+                                '/customer/account-setting'
+                            ),
+                        }"
+                    >
+                        <i class="fa-solid fa-gear w-5 h-5 mr-3"></i>
+                        Account Setting
+                    </Link>
+                    <Link
+                        :href="route('logout')"
+                        method="post"
+                        class="flex items-center w-full p-3 rounded-md hover:bg-gray-700 transition-colors duration-200 text-white"
+                    >
+                        <i
+                            class="fa-solid fa-arrow-right-from-bracket mr-3"
+                        ></i>
+                        Logout
+                    </Link>
                 </li>
             </ul>
         </nav>

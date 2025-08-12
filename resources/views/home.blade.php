06fad6a082e74a46303fb8471d10a5e528d5c9ee
Qurayshkhan <96570166+Qurayshkhan@users.noreply.github.com>
2025-08-12 15:44:20 +0500

    fix: home template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -66,9 +66,9 @@
                                 address</label>
                             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                 <!-- <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                                                                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                                                                                                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                                                                                                            </svg> -->
+                                                                                                                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                                                                                                                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                                                                                                                </svg> -->
                             </div>
                             <input
                                 class="revue-form-field bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black focus:border-black block w-72 pl-10 p-2.5"
@@ -90,7 +90,7 @@
             <div class="flex justify-between items-end">
                 <img src="assets/image/home/us-with-plane.svg" alt="U.S. illustration" style='width:300px'>
                 <img src="assets/image/home/island.svg" alt="Island illustration" class="city hidden">
-                <img src="assets/image/home/svg/home2.svg" alt="EU illustration" class="city">
+                <img src="assets/image/home/home2.svg" alt="EU illustration" class="city">
             </div>
         </div>
         <img src="{{ asset('assets') }}/image/home/box.svg" alt="floating icon"
